447e94b8fa7c77a0005a10b28335b6e564d0090d
Kristina Tomic <kristinatomic@Kristinas-MacBook-Pro.local>
2017-09-21 11:09:10 +0200

    Lecture2

---
+++ wp-content/themes/alumni/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<nav>
+	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>	
+</nav>
---
+++ wp-content/themes/alumni/index.php
@@ -0,0 +1,4 @@
+<?php get_header()?>
+Hello World
+
+<?php get_footer()?>
